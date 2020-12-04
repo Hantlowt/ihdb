803901b5b77430ab7702e3028a89d524c473c82d
Hantlowt <lhote.antoine@gmail.com>
2020-12-04 10:14:48 +0100

    Add setup.py for package

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="ihdb",
+    version="0.0.1",
+    description="ihdb",
+    long_description="I Hate Database. It's like SQLITE but with graph. Perfect for prototype. No tables, no documents",
+    url="https://github.com/Hantlowt/ihdb",
+    packages=setuptools.find_packages(),
+    py_modules=['ihdb']
+)
