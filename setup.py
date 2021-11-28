83f178ae21076117fbd006124ba3a71ef736b89d
Hantlowt <lhote.antoine@gmail.com>
2021-11-28 19:08:26 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ihdb",
-    version="0.0.9",
+    version="1.0.0",
     description="ihdb",
     long_description="I Hate Database. It's like SQLITE but with graph. Perfect for prototype. No tables, no documents",
     url="https://github.com/Hantlowt/ihdb",
